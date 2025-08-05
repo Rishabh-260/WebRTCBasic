53d389811c774852b92d8ac73093804d4b069cac
Rishabh Kumar <kumarrishabh2604@gmail.com>
2025-08-05 18:25:47 +0530

    Update webrtc.js

--- webrtc.js
+++ webrtc.js
@@ -6,7 +6,18 @@ let peerConnection;
 
 function startCall() {
     const configuration = {
-        'iceServers': [{ 'urls': 'stun:stun.example.com' }]
+        'iceServers': [
+                        { urls: "stun:stun.l.google.com:19302" },
+                        { urls: "stun:stun.l.google.com:5349" },
+                        { urls: "stun:stun1.l.google.com:3478" },
+                        { urls: "stun:stun1.l.google.com:5349" },
+                        { urls: "stun:stun2.l.google.com:19302" },
+                        { urls: "stun:stun2.l.google.com:5349" },
+                        { urls: "stun:stun3.l.google.com:3478" },
+                        { urls: "stun:stun3.l.google.com:5349" },
+                        { urls: "stun:stun4.l.google.com:19302" },
+                        { urls: "stun:stun4.l.google.com:5349" }
+                    ]
     };
     peerConnection = new RTCPeerConnection(configuration);
 
